a4b446604eda2708fd8765d502e00c471cddbbc2
jericho909 <omerahmetyucel@gmail.com>
2023-07-26 16:15:12 +0300

    added different sized grids and rainbow mode

--- js-file.js
+++ js-file.js
@@ -1,25 +1,107 @@
-const gridContainer = document.getElementById("gridContainer");
+function addClassicHoverBehavior() {
+    const gridSquares = document.querySelectorAll(".grid-square");
+    gridSquares.forEach((gridSquare) => {
+      gridSquare.addEventListener("mouseenter", function (e) {
+        e.target.style.backgroundColor = "black";
+      });
+    });
+  }
 
 
-for (let h = 0; h < 16; h++) {
-    let gridSquare = document.createElement("div");
-    gridSquare.setAttribute(
-      "style",
-      "width: 60px; height: 60px; background-color:white;border:none;outline:1px solid #000"
-    );
-    gridSquare.classList.add("grid-square");
-    gridContainer.appendChild(gridSquare);
+function createGrid(size) {
+    gridContainer.innerHTML = ''; // Clear existing grid
+    if (size === 16) {
+    for (let j = 0; j < (size * size) - 1; j++) {
+        let gridSquare = document.createElement("div");
+        gridSquare.setAttribute(
+          "style",
+          "width: 36px; height: 36px; background-color:white;border: none; outline: 1px solid #000; margin: 0;"
+        );
+        gridSquare.classList.add("grid-square");
+        gridContainer.appendChild(gridSquare);
+      }
+    } else if (size === 32) {
+        for (let j = 0; j < (size * size) - 9; j++) {
+            let gridSquare = document.createElement("div");
+            gridSquare.setAttribute(
+              "style",
+              "width: 18px; height: 18px; background-color:white;border: none; outline: 1px solid #000; margin: 0;"
+            );
+            gridSquare.classList.add("grid-square");
+            gridContainer.appendChild(gridSquare);
+          }
+    } else if (size === 64) {
+        for (let j = 0; j < (size * size) - 49; j++) { //don't ask
+            let gridSquare = document.createElement("div");
+            gridSquare.setAttribute(
+              "style",
+              "width: 9px; height: 9px; background-color:white;border: none; outline: 1px solid #000; margin: 0;"
+            );
+            gridSquare.classList.add("grid-square");
+            gridContainer.appendChild(gridSquare);
+          }
+    }
+
+    addClassicHoverBehavior();
 }
+  
+createGrid(16);
+
+document.getElementById("btn16x16").addEventListener("click", function() {
+    createGrid(16);
+  });
+  
+document.getElementById("btn32x32").addEventListener("click", function() {
+    createGrid(32);
+  });
+  
+document.getElementById("btn64x64").addEventListener("click", function() {
+    createGrid(64);
+  });
+
+const clearButton = document.getElementById("buttonThree");
 
 
-const gridSquares = document.querySelectorAll(".grid-square");
-
-gridSquares.forEach((gridSquare) => {
-    gridSquare.addEventListener("mouseenter", function(e) {
-        e.target.style.backgroundColor = "black"
+clearButton.addEventListener("click", function(e) {
+    const gridSquares = document.querySelectorAll(".grid-square");
+    gridSquares.forEach((gridSquare) => {
+        gridSquare.style.backgroundColor = "white";
     });
 });
 
+const rainbowButton = document.getElementById("buttonTwo");
+rainbowButton.addEventListener("click", function () {
+  const gridSquares = document.querySelectorAll(".grid-square");
+  gridSquares.forEach((gridSquare) => {
+    gridSquare.addEventListener("mouseenter", function (e) {
+      const randomColor = getRandomRainbowColor();
+      e.target.style.backgroundColor = randomColor;
+    });
+  });
+});
+
+function getRandomRainbowColor() {
+  const rainbowColors = [
+    "red", "orange", "yellow", "green", "blue", "purple"
+  ];
+  const randomIndex = Math.floor(Math.random() * rainbowColors.length);
+  return rainbowColors[randomIndex];
+}
+
+const classicButton = document.getElementById("buttonOne");
+classicButton.addEventListener("click", function () {
+  addClassicHoverBehavior();
+});
+
+  
+
+
+
+
+
+
+
+  
 
 
 
